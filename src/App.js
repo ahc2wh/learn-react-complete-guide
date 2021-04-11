17b704ea21a5e511afc4bbeaf3928a26825b52ee
ahc2wh <ahc2wh@virginia.edu>
2021-04-11 01:29:25 -0400

    44, hook functional component version of App, will rollback in next commit to traditional class based component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,62 @@
 import './App.css';
-import React, { Component } from 'react'; // less nested Component this way, vs extends React.Component
+import React, { useState } from 'react'; // less nested Component this way, vs extends React.Component
 import Person from './Person/Person.js';  // relative path , .js extension not needed but still works
 
+// function body has no render method, onlny return JSX
+// hooks react 16.8+
+const App = props => {
+  // useState returns an array with 2 elements
+  // 1st element in stateArray is always current state
+  // 2nd element in stateArray is a function that is used to update the state
+  // modern javascript feature; array destructuring
+  // const [ ] = ARRAY Here  // allows pulling elements out of array from right side/ array
+  // personsState gives us data
+  // setPersonsState allows us to set a new state
+  const [ personsState, setPersonsState]  = useState({
+    persons: [
+      { name: 'Sam', age: 11 },
+      { name: 'Larry', age: 44 },
+      { name: 'Rachel', age: 21 }
+    ],
+    otherState: 'some other value'
+  });
+
+  // can have many useStates
+  const [ otherState, setOtherState ] = useState('some other value')
+
+  //NOTE: if using REACT hooks, the setState of class based components does not merges new onto old state
+  // instead it replaces old state with new state. you have to manually include the old state you want to keep
+  // open browser's console to see console.log
+  console.log(personsState, otherState)
+  // should see that otherState doesn't change but personState does onClick
+
+
+  // you can have functions inside functions (switchNameHandler in app)
+  const switchNameHandler = () => {
+    //console.log('Was clicked')
+    // NOTE: to keep old otherState, don't use setOtherState
+    setPersonsState({
+      persons: [
+        { name: 'Samuel', age: 11 },
+        { name: 'Larry the Cable Guy', age: 44 },
+        { name: 'Rachelllll', age: 21 }
+      ]
+    });
+  }
+  
+  return (
+  <div className="App">
+    <h1>Hi, I'm a React App!!!</h1>
+    <button onClick={switchNameHandler}>Switch Name</button>
+    <Person name={personsState.persons[0].name} age={personsState.persons[0].age} />
+    <Person name={personsState.persons[1].name} age={personsState.persons[1].age}> He also has stock options </Person>
+    <Person name={personsState.persons[2].name} age={personsState.persons[2].age}/>
+  </div>
+  );
+}
+
+// old class based component
+/*
 class App extends Component {
 
   state = {
@@ -10,13 +65,12 @@ class App extends Component {
       { name: 'Larry', age: 44 },
       { name: 'Rachel', age: 21 }
     ],
-    otherState: 'some other value'
+    otherState: 'some other value that'
   }
   // using ES6 arrow helps keep from confusing scope of this at App and this in switchNameHandler
+  // switchNameHandler = property with an executable function, don't put () at end when using, else it executes immediately
   switchNameHandler = () => {
     //console.log('Was clicked')
-    this.state.persons[0].name = 'Samuel'; //this line doesnt work, react will not recognize changing the state (mutate) directtly
-    //setState is from extends Component
     this.setState({
       persons: [
         { name: 'Samuel', age: 11 },
@@ -24,29 +78,19 @@ class App extends Component {
         { name: 'Rachelllll', age: 21 }
       ]
     })
-    // setState takes object as arguement and merges whatever added in setState with this.states
-    // merging old state with new one, override what got changed
-  }
-  // if assign function as value, then the property becomes a method.
-  // switchNameHandler becomes a property with an executable function within a property
+  };
 
-  // <button/>'s onClick is JSX version of javascripts onclick
-  // see other events: https://reactjs.org/docs/events.html#supported-events 
   render() {
        return (
         <div className="App">
           <h1>Hi, I'm a React App!!!</h1>
           <button onClick={this.switchNameHandler}>Switch Name</button>
-          <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
-          <Person name={this.state.persons[1].name} age={this.state.persons[1].age}> He also has stock options </Person>
-          <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
+          <Person name={personsState.persons[0].name} age={personsState.persons[0].age} />
+          <Person name={personsState.persons[1].name} age={personsState.persons[1].age}> He also has stock options </Person>
+          <Person name={personsState.persons[2].name} age={personsState.persons[2].age}/>
         </div>
        );
-       // by adding parenthesis this.switchNameHandler() executes immediately once React reacts to DOM (vs waiting for you to click)
-       // the parenthesis executes the function. we only want to send a reference that holds a function
-       // Person has 2 properties + 1 children property
   }
-}
+}*/
 
 export default App;
-// ES6 feature - if you import this whole file, you simply import this class becuase its the default export
