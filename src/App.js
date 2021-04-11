affee119dc403922897dec3b151b5c2e9030e207
ahc2wh <ahc2wh@virginia.edu>
2021-04-11 00:42:58 -0400

    42, event->state updates DOM success

--- src/App.js
+++ src/App.js
@@ -9,21 +9,34 @@ class App extends Component {
       { name: 'Sam', age: 11 },
       { name: 'Larry', age: 44 },
       { name: 'Rachel', age: 21 }
-    ]
+    ],
+    otherState: 'some other value'
   }
   // using ES6 arrow helps keep from confusing scope of this at App and this in switchNameHandler
   switchNameHandler = () => {
-    console.log('Was clicked')
+    //console.log('Was clicked')
+    this.state.persons[0].name = 'Samuel'; //this line doesnt work, react will not recognize changing the state (mutate) directtly
+    //setState is from extends Component
+    this.setState({
+      persons: [
+        { name: 'Samuel', age: 11 },
+        { name: 'Larry the Cable Guy', age: 44 },
+        { name: 'Rachelllll', age: 21 }
+      ]
+    })
+    // setState takes object as arguement and merges whatever added in setState with this.states
+    // merging old state with new one, override what got changed
   }
   // if assign function as value, then the property becomes a method.
   // switchNameHandler becomes a property with an executable function within a property
 
   // <button/>'s onClick is JSX version of javascripts onclick
+  // see other events: https://reactjs.org/docs/events.html#supported-events 
   render() {
        return (
         <div className="App">
           <h1>Hi, I'm a React App!!!</h1>
-          <button onClick={this.switchNameHandler()}>Switch Name</button>
+          <button onClick={this.switchNameHandler}>Switch Name</button>
           <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
           <Person name={this.state.persons[1].name} age={this.state.persons[1].age}> He also has stock options </Person>
           <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
