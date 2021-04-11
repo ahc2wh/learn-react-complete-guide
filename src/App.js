a6da386f3bb0db517c71cf173169c850237c3d9a
ahc2wh <ahc2wh@virginia.edu>
2021-04-10 21:17:26 -0400

    29, added comments on App render

--- src/App.js
+++ src/App.js
@@ -2,8 +2,12 @@ import './App.css';
 
 function App() {
   return (
+    // this content is not html but JSX (react self made term)
+    // JSX will auto transcribe it to javascript
+    // allows writing html in quotation marks
     <h1>Hi I'm a react app, removing some boiler plate</h1>
   );
 }
 
 export default App;
+// ES6 feature - if you import this whole file, you simply import this class becuase its the default export
