22e691e71d6df2d085f0e5d23c8eb409c508e12b
ahc2wh <ahc2wh@virginia.edu>
2021-04-10 23:26:55 -0400

    32, Person component created

---
+++ src/Person/Person.js
@@ -0,0 +1,9 @@
+import '../App.css'; //step back one directory back to src
+import React from 'react'; // component not needed, making a function
+
+// ES6 usage
+const person = () => {
+    return <p>I'm a Person!</p>
+}
+
+export default person; //exports this const person with a person function 
